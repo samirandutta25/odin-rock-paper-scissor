27ab468789bd1531a4ed476abf3af1ed5fc2d251
Samiran Dutta <samiran.dutta71@gmail.com>
2025-04-23 18:15:09 +0530

    Adding GUI instead of playing in Console

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,5 +1,17 @@
+const rockButton = document.querySelector('.rock-icon');
+const paperButton = document.querySelector('.paper-icon');
+const scissorsButton = document.querySelector('.scissors-icon');
+
+const computerScoreDiv = document.querySelector('.computer-score');
+const playerScoreDiv = document.querySelector('.player-score');
+
+const resultDiv = document.querySelector('.result');
+
+let playerScore = computerScore = 0;
 
 let capitalize = (str) => str[0].toUpperCase() + str.substring(1).toLowerCase();
+
+const controller = new AbortController();
 
 function getComputerChoice(){
     // 0 -> rock
@@ -14,31 +26,42 @@ function getComputerChoice(){
         return "scissors";
 }
 
-function getHumanChoice(){
-    return prompt("Rock/Paper/Scissors !!! Choose: ").toLowerCase();
+function updateScores(){
+    computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+    playerScoreDiv.textContent = `Player Score: ${playerScore}`;
+    if( computerScore === 5){
+        const innerDiv = document.createElement("div");
+        innerDiv.textContent = "Computer Won!! --- Refresh to start a New Game.";
+        resultDiv.append(innerDiv);
+        controller.abort();
+    }else if(playerScore === 5){
+        const innerDiv = document.createElement("div");
+        innerDiv.textContent = "You Won!! --- Refresh to start a New Game.";
+        resultDiv.append(innerDiv);
+        controller.abort();
+    }
 }
 
 function playRound(humanChoice, computerChoice) {
     
-    console.log(`Computer chose: ${capitalize(computerChoice)} --- Player chose: ${capitalize(humanChoice)}`);
+    resultDiv.textContent = `Player chose: ${capitalize(humanChoice)} --- Computer chose: ${capitalize(computerChoice)}`;
 
     if(humanChoice === computerChoice) return;
 
     if( humanChoice === 'rock'){
-        ( computerChoice === 'paper') ? computerScore++ : humanScore++;
+        ( computerChoice === 'paper') ? computerScore++ : playerScore++;
     }
     else if( humanChoice === 'paper' ){
-        ( computerChoice === 'scissors') ? computerScore++ : humanScore++;
+        ( computerChoice === 'scissors') ? computerScore++ : playerScore++;
     }
     else if( humanChoice === 'scissors' ){
-        ( computerChoice === 'rock') ? computerScore++ : humanScore++;
+        ( computerChoice === 'rock') ? computerScore++ : playerScore++;
     }
+
+    updateScores();
 }
 
-let humanScore = computerScore = 0; 
-  
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
-console.log(`Scores - Computer: ${computerScore} --- Player: ${humanScore}`);
+rockButton.addEventListener("click", () =>  playRound("rock", getComputerChoice()), { signal: controller.signal });
+paperButton.addEventListener("click", () =>  playRound("paper", getComputerChoice()), { signal: controller.signal });
+scissorsButton.addEventListener("click", () =>  playRound("scissors", getComputerChoice()), { signal: controller.signal });
